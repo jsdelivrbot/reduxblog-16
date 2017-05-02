4064e4ef59a907588d6518a11e44a886983b5745
petrios888 <appeter22@yahoo.com>
2017-05-02 15:30:17 -0400

    Gamma

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   componentWillMount(){
     this.props.fetchPosts();
   }
+
   render () {
     return (
-      <div> List of blog posts </div>
+      <div>
+        <div className= "text-xs-right">
+          <Link to="posts/new" className="btn btn-primary">
+            God is Good
+          </Link>
+        </div>
+          List of blog posts
+      </div>
     );
   }
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({ fetchPosts}, dispatch );
-}
 
 
-export default connect(null, mapDispatchToProps)(PostsIndex);
+export default connect(null, { fetchPosts }) (PostsIndex);
